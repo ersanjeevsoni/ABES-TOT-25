9eeda20cc7fca47f42d7a8893b65c94a0456f6a1
Ersanjeev Soni <ersanjeevsoni@gmail.com>
2025-08-20 11:14:46 +0530

    function

--- Day4_JS/index.js
+++ Day4_JS/index.js
@@ -1,4 +1,109 @@
-var a=18;
+/* var a=18;
 console.log("a= "+12);
 console.log(a+40);
-console.log(typeof(a));
+console.log(typeof(a)); */
+
+// variable declaration
+
+/* var a=17;
+if(a>10){
+    let a=30;
+    console.log(a);
+}
+console.log(a); */
+
+
+/* let b=4; 
+//b=4;
+console.log(b);
+const c=13; 
+// c=13; //error
+console.log(c); */
+
+/* let a=12;
+let b="12"; */
+/* if(a==b){
+    console.log(true);
+} */
+
+    //equation
+/* if(a===b){
+    console.log(true);
+}
+else{
+    console.log(false);
+} */
+
+    // simple function defination and calling
+
+  /*   function sum(a,b){
+        return a+b;
+    }
+    let val=sum(12,20);
+    console.log("sum="+val); */
+
+     // simple function calling first then defination 
+/* let val=sum(12,20);
+    console.log("sum="+val);
+    function sum(a,b){
+        return a+b;
+    } */
+
+        //function expresssion
+
+       /*  const data=function(){
+            console.log("hello");
+        }
+        data(); */
+        //usnig arrow function
+/*         const data=()=>{
+            console.log("hi");
+        }
+        data();
+
+
+        //passing parameter
+
+        const sum=(a,b)=>{
+            return a+b;
+        }
+        console.log(sum(4,3));
+
+        const add=(a,b)=>a+b;
+        console.log(add(23,45));
+        //IIFE
+
+        (()=>{
+            console.log("hello IIFE");
+        })();
+    //default parameter function
+    function hi(msg="hello"){
+        console.log(msg);
+    }
+    hi(); */
+    //conditional statement
+
+  /*   function selectLang(lang){
+        let data;
+        if(lang=="java"){
+            function javaCompiler(){
+               console.log("JAVA Compiler");;
+            }
+            data=javaCompiler();
+        }
+        else if(lang=="c"){
+            function cCompiler(){
+                console.log("C Compiler");
+            }
+            data=cCompiler();
+        }
+        else{
+            console.log("Compiler not found");
+        }
+
+    }
+    selectLang(); */
+
+    
+
+    
